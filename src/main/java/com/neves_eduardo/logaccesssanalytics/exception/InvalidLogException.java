d195e23c82c5ac98f33fcb27b34febdc32b74c4a
neves-eduardo <eduardoneves858@gmail.com>
2020-04-08 04:35:34 -0300

    feat: adding exceptions

---
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/exception/InvalidLogException.java
@@ -0,0 +1,7 @@
+package com.neves_eduardo.logaccesssanalytics.exception;
+
+public class InvalidLogException extends RuntimeException{
+    public InvalidLogException(String msg) {
+        super(msg);
+    }
+}
