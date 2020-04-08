ebc096fececc0ddfb2a2a0897ce38bff7532b4b2
neves-eduardo <eduardoneves858@gmail.com>
2020-04-08 04:32:59 -0300

    feat: adding Analyzer interface &  class

---
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/service/PetShopLogAnalyzer.java
@@ -0,0 +1,10 @@
+package com.neves_eduardo.logaccesssanalytics.service;
+
+import com.neves_eduardo.logaccesssanalytics.dto.Log;
+
+public class PetShopLogAnalyzer {
+
+    public void analyzeLog (Log log) {
+        //TODO PUBLISHING TO INFLUXDB
+    }
+}
