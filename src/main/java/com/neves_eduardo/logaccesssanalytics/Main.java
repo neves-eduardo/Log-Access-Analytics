76b9acf7a68ca2d7a4090d64db8a56a386d24c8b
neves-eduardo <eduardoneves858@gmail.com>
2020-04-08 04:34:59 -0300

    chore: adding main class

---
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/Main.java
@@ -0,0 +1,8 @@
+package com.neves_eduardo.logaccesssanalytics;
+
+import com.neves_eduardo.logaccesssanalytics.service.PetShopLogDecoder;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
