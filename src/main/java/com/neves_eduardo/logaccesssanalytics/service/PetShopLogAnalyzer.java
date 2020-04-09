834750fba1b8169d32743f56c1520c252a117eed
neves-eduardo <eduardoneves858@gmail.com>
2020-04-08 21:04:25 -0300

    feat: adding analyze method

--- src/main/java/com/neves_eduardo/logaccesssanalytics/service/PetShopLogAnalyzer.java
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/service/PetShopLogAnalyzer.java
@@ -1,10 +1,16 @@
 package com.neves_eduardo.logaccesssanalytics.service;
 
+import com.neves_eduardo.logaccesssanalytics.dao.Publisher;
 import com.neves_eduardo.logaccesssanalytics.dto.Log;
 
 public class PetShopLogAnalyzer {
+    private Publisher publisher;
+
+    public PetShopLogAnalyzer(Publisher publisher) {
+        this.publisher = publisher;
+    }
 
     public void analyzeLog (Log log) {
-        //TODO PUBLISHING TO INFLUXDB
+        publisher.publish(log);
     }
 }
