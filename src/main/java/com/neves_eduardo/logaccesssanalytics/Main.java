ee53597b2ac74e781abc13926ff281071b8f310f
neves-eduardo <eduardoneves858@gmail.com>
2020-04-20 21:29:58 -0300

    feat: adding starting server method to the Main class

--- src/main/java/com/neves_eduardo/logaccesssanalytics/Main.java
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/Main.java
@@ -1,7 +1,28 @@
 package com.neves_eduardo.logaccesssanalytics;
 
-public class Main {
-    public static void main(String[] args) {
+import com.neves_eduardo.logaccesssanalytics.dto.Log;
+import com.neves_eduardo.logaccesssanalytics.server.JettyServer;
+import org.eclipse.jetty.server.Server;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        JettyServer jettyServer = new JettyServer();
+        applicationContext.setConfigLocation("com.neves_eduardo.logaccesssanalytics.config.AppConfig");
+        Server server = jettyServer.createServer(8888,applicationContext);
+        server.start();
+        server.join();
     }
+
 }
