42791934134318af2a3facb4bfaa4de636f9fa4b
neves-eduardo <eduardoneves858@gmail.com>
2020-04-20 21:30:59 -0300

    chore: adding mvc annotation to config

--- src/main/java/com/neves_eduardo/logaccesssanalytics/config/AppConfig.java
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/config/AppConfig.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.neves_eduardo.logaccesssanalytics")
 public class AppConfig {
     @Bean
