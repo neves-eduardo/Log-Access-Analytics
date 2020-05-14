9782986e4e0fd9249cffe6eacb33db3e9696db43
neves-eduardo <eduardoneves858@gmail.com>
2020-05-14 14:18:07 -0300

    feat: adding PropertyReader class

---
+++ src/main/java/com/neves_eduardo/logaccesssanalytics/config/PropertiesReader.java
@@ -0,0 +1,28 @@
+package com.neves_eduardo.logaccesssanalytics.config;
+
+import com.neves_eduardo.logaccesssanalytics.exception.CannotLoadConfigsException;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@Component
+public class PropertiesReader {
+    private static final String CONFIG_FILE_PATH = "config.properties";
+    private Properties properties;
+
+    public PropertiesReader() {
+        this.properties = new Properties();
+    }
+
+    public String loadProperty(String property) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH);
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new CannotLoadConfigsException("Can not load Configs");
+        }
+        return properties.getProperty(property);
+    }
+}
